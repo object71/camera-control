d58dbdf35298f60e4e28851aedf4cf2f4eabdf14
Hristo Stamenov <capitane71@gmail.com>
2018-07-08 02:17:07 +0300

    Added some general files to the project

---
+++ src/main/java/com/github/object71/cameracontrol/common/Constants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.object71.cameracontrol.common;
+
+/**
+ *
+ * @author hristo
+ */
+public class Constants {
+    public static void getValue() {
+        
+    }
+}
